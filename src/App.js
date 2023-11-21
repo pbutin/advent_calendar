1acaee1b84e8e459a27520e25214e912bf26bd99
Pierrick Butin <pierrick.butin@lamie-mutuelle.fr>
2023-11-21 12:16:44 +0100

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import bg from './img/image.png';
-// import tiles from './data/messages.json';
 import tiles from './data/messages_lamie.json';
 import SnowFall from 'react-snowfall';
 import Tile from './Tile.js'
@@ -21,7 +20,7 @@ function App() {
         <div className="bg-holder" style={{ backgroundImage: `url(${bg})` }}>
           <ul id="adventDoors">
             {tiles.map(tile => {
-              return <Tile jour={tile.jour} text={tile.text} image={tile.url} /> 
+              return <Tile jour={tile.jour}/> 
             })}
           </ul>
         </div>
--- src/Tile.js
+++ src/Tile.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
 import noeud from './img/noeud.png';
 
-function Tile(props) {
+function Tile({jour}) {
     const [isFlipped, setIsFlipped] = useState(false);
 
     function toogle() {
       var a = new Date();
-      var b = new Date(2023, 11, props.jour, 12, 0, 0, 0);
+      var b = new Date(2023, 11, jour, 12, 0, 0, 0);
       if (b-a < 0)
         setIsFlipped(!isFlipped)
     }
@@ -16,9 +16,9 @@ function Tile(props) {
     <div className='tileContainer'>
         <img src={noeud} alt="noeud" className={'noeud ' + (isFlipped ? "z-index" : "")} onClick={() => toogle()}/>
         <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-            <li className="front" onClick={() => toogle()}>{props.jour}</li>
+            <li className="front" onClick={() => toogle()}>{jour}</li>
             <li className="back" >
-              <img className='tileImg' src={require('./img/days/' + props.jour + '.png')} alt={'Erreur chargement jour ' + props.jour}/>
+              <img className='tileImg' src={require('./img/days/' + jour + '.png')} alt={'Erreur chargement jour ' + jour}/>
             </li>
         </ReactCardFlip>
     </div>
